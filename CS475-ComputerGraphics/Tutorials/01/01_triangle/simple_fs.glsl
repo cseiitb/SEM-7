7484a7cfd5ecfaf8ebbcf3c44509d97c0d1c113e
cseiitb <dummy@abc.com>
2019-04-19 13:54:22 +0530

    first commit

---
+++ CS475-ComputerGraphics/Tutorials/01/01_triangle/simple_fs.glsl
@@ -0,0 +1,8 @@
+#version 400
+
+out vec4 frag_colour1;
+
+void main () 
+{
+  frag_colour1 = vec4 (1.0, 0.0, 1.0, 1.0);
+}
---
+++ CS475-ComputerGraphics/Tutorials/01/01_triangle/simple_fs2.glsl
@@ -0,0 +1,8 @@
+#version 400
+
+out vec4 frag_colour2;
+
+void main () 
+{
+  frag_colour2 = vec4 (0.0, 1.0, 1.0, 1.0);
+}
